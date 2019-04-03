a19e662af26a8937caa89322604be46d4ca0e2ad
AndreBtt <bittencourtandre@hotmail.com>
2019-04-03 08:45:53 -0300

    generate next matches almost done!

--- api/components/nextmatch/controller.go
+++ api/components/nextmatch/controller.go
@@ -6,8 +6,202 @@ import (
 	"sort"
 )
 
+type Match struct {
+	Team1 Team
+	Team2 Team
+}
+
+type Team struct {
+	Name  string
+	Times []bool
+	Group int
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func getTeamsGroup(allTeams []Team, groupNumber int) []Team {
+	var teams []Team
+	for _, elem := range teams {
+		if elem.Group == groupNumber {
+			teams = append(teams, elem)
+		}
+	}
+	return teams
+}
+
+func combinationMatches(combMatches *[][]Match, currMatch []Match, mask int, pairs int, teams []Team) {
+
+	if 2*pairs == len(teams) {
+		// sort using unique data (team's name) from both teams in the match
+		// this will be helpeful to take off equal elements in the future
+		sort.Slice(currMatch, func(i, j int) bool {
+			var comp1 string
+			var comp2 string
+			if currMatch[i].Team1.Name < currMatch[i].Team2.Name {
+				comp1 = currMatch[i].Team1.Name + currMatch[i].Team2.Name
+			} else {
+				comp1 = currMatch[i].Team2.Name + currMatch[i].Team1.Name
+			}
+
+			if currMatch[j].Team1.Name < currMatch[j].Team2.Name {
+				comp2 = currMatch[j].Team1.Name + currMatch[j].Team2.Name
+			} else {
+				comp2 = currMatch[j].Team2.Name + currMatch[j].Team1.Name
+			}
+
+			return comp1 < comp2
+		})
+		*combMatches = append(*combMatches, currMatch)
+		return
+	}
+
+	for i := 0; i < len(teams); i++ {
+		if ((1 << uint(i)) & mask) == 0 {
+			// free position
+			for j := i + 1; j < len(teams); j++ {
+				if ((1 << uint(j)) & mask) == 0 {
+					// free position
+					// team with index i will play against team with index j
+					var m Match
+					m.Team1 = teams[i]
+					m.Team2 = teams[j]
+					currMatch = append(currMatch, m)
+
+					// set that these positions are not available
+					newMask := mask
+					newMask = newMask | (1 << uint(i))
+					newMask = newMask | (1 << uint(j))
+
+					combinationMatches(combMatches, currMatch, newMask, pairs+1, teams)
+					currMatch = currMatch[:len(currMatch)-1]
+				}
+			}
+		}
+	}
+}
+
+func getCombinationMatches(teams []Team) [][]Match {
+	var combMatches [][]Match
+	var currMatch []Match
+	combinationMatches(&combMatches, currMatch, (1 << uint(len(teams))), 0, teams)
+
+	// taking off equal elements
+	equal := make(map[string]bool)
+	var correctComb [][]Match
+	for _, comb := range combMatches {
+		var key string
+		// create a unique key for the matches combination
+		for _, match := range comb {
+			key += match.Team1.Name
+			key += match.Team2.Name
+		}
+		if _, ok := equal[key]; !ok {
+			// this key does't exist
+			correctComb = append(correctComb, comb)
+			equal[key] = true
+		}
+	}
+
+	return correctComb
+}
+
+func getTimePermutation(teamSize int, timeQt int) [][]int {
+	return nil
+}
+
+// timeQt = how many times we have
+// time = bit mask time where 0 is available time, start with (1 << timeQt)
+func solve(groupNumber int, timeQt int, time int, allTeams []Team) int {
+	if groupNumber == -1 {
+		return 0
+	}
+
+	teamsGroup := getTeamsGroup(allTeams, groupNumber)
+
+	matchCombination := getCombinationMatches(teamsGroup)
+
+	// len(matchCombination[0]) indicates how many matches we have
+	// this return all combinations of matches and times
+	// each position can be either -1 where indicates that there is no match in this position
+	// or greater then -1 which is the matche's index number
+	timePermutation := getTimePermutation(len(matchCombination[0]), timeQt)
+
+	bigAns := 0
+
+	for _, combMatch := range matchCombination {
+
+		ans := 0
+
+		for _, combTime := range timePermutation {
+			ok := true
+			for h, t := range combTime {
+				// h is the time (hour)
+				// t is the match index inside combMatch
+				if t != -1 {
+					// match with index t is trying to play on time h
+					if ((1 << uint(h)) & time) > 0 {
+						// this time is not available
+						ok = false
+						break
+					}
+				}
+			}
+			if !ok {
+				// this combination of times is not available
+				continue
+			}
+
+			points := 0
+			newTime := time
+
+			for h, t := range combTime {
+				if t != -1 {
+					// match with index t plays on time h
+					if combMatch[t].Team1.Times[h] == true {
+						// check if the first team of the match wants to play on time h
+						points++
+					}
+					if combMatch[t].Team2.Times[h] == true {
+						// check if the second team of the match wants to play on time h
+						points++
+					}
+					// set that time h is no avaible more
+					newTime = (time | (1 << uint(h)))
+				}
+			}
+
+			ans = max(ans, solve(groupNumber-1, timeQt, newTime, allTeams)+points)
+		}
+
+		bigAns = max(bigAns, ans)
+	}
+
+	return bigAns
+}
+
 func GenerateNextMatches(db *sql.DB) error {
 	// [grupo][1<<horarios]
+
+	var teams []Team
+
+	for i := 0; i < 6; i++ {
+		s := fmt.Sprintf("t%d", i+1)
+		t := Team{
+			Name: s,
+		}
+		teams = append(teams, t)
+	}
+
+	tt := getCombinationMatches(teams)
+
+	for _, elem := range tt {
+		fmt.Println(elem)
+	}
 
 	return fmt.Errorf("Not implemented yet")
 }
